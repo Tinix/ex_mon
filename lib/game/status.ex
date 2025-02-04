0e60b718ce322d0418a73cd678f846ababacbce3
Daniel Tinivella <dtinivella@gmail.com>
2025-02-04 13:08:15 -0300

    add actions and game/actions/attack

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -4,6 +4,10 @@ defmodule ExMon.Game.Status do
   def print_round_message() do
     IO.puts("\n===== The game is started! =====\n")
     IO.inspect(Game.info())
-    IO.puts("Good luck!")
+    IO.puts("===== Good luck! =====\n")
+  end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n===== Invalid move: #{move}! =====\n")
   end
 end
